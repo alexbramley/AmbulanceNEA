69458d68b3d699bbf29732d78abe0c44bad9c9ee
alexbramley <epicmrbramley@gmail.com>
2025-12-05 11:37:51 +0000

    we got the functionality on the entitymanager finally it worsk

--- entities.py
+++ entities.py
@@ -21,18 +21,19 @@ class EntityManager:
             if cls._secure_connection != None:
                 new_conn_msg = cls._secure_connection.get_most_recent_message()
                 if cls._newest_conn_msg != new_conn_msg:
+                    print("we got a brand new message")
                     cls._newest_conn_msg = new_conn_msg
                     cls._handle_conn_msg(cls._newest_conn_msg)
 
     @staticmethod
     def start_master():
+        EntityManager._master_active = True
         EntityManager.master_thread = threading.Thread(target=EntityManager._master)
         EntityManager.master_thread.start()
-        _master_active = True
 
     @staticmethod
     def end_master():
-        _master_active = False
+        EntityManager._master_active = False
         if EntityManager.master_thread != None:
             EntityManager.master_thread.join()
 
@@ -110,52 +111,8 @@ class EntityManager:
 
 
 class Entity(object):
-    def __init__(self, position:vectors.Vector2, socket_instruction_set:dict):
+    def __init__(self, position:vectors.Vector2):
         self.position = position
-        self.secure_connection = None
-        self.socket_instruction_set = socket_instruction_set
-        self._most_recent_socket_message = ""
-
-        self._master_thread = threading.Thread(target=self._master)
-        self._master_thread.start()
-
-    def _master(self):
-        while True:
-            if self.secure_connection != None:
-                new_socket_message = self.secure_connection.get_most_recent_message()
-                if new_socket_message != self._most_recent_socket_message:
-                    
-                    self._most_recent_socket_message = new_socket_message
-
-                    self._handle_socket_message(self._most_recent_socket_message)
-
-    def set_secure_connection(self, new_secure_connection:ss.SecureConnection):
-        self.secure_connection = new_secure_connection
-
-    def _handle_socket_message(self, message):
-        """Gets triggered when a the secure_socket object receives a new connection"""
-        if message[0] != "<":
-            print(Exception("No command recognised!!"))
-            return
-        
-        command = ""
-        reading_command = True
-        for letter in message:
-            if reading_command:
-                if letter == ">":
-                    reading_command = False
-                    continue
-                if letter == "<":
-                    continue
-                command += letter
-
-        print(f"received command: {command}")
-
-
-    def send_socket_message(self, message):
-        if self.secure_connection == None:
-            return Exception("No secure_connection object to send with")
-        self.secure_connection.send(message)
 
 
 
