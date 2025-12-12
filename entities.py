aa6c2cd9196ead489a62739fbafecb14c75dbe1e
alexbramley <epicmrbramley@gmail.com>
2025-12-12 12:08:05 +0000

    servermanager more or less works

--- entities.py
+++ entities.py
@@ -12,6 +12,9 @@ class ConnectionManager(object):
         self._newest_conn_msg = ""
         self.master_thread = None
         self._master_active = False
+        self._newest_conn_command_data = []
+        self._newest_conn_argument_data = []
+        self.new_message_flag = False
 
     
     def _master(self):
@@ -22,12 +25,13 @@ class ConnectionManager(object):
                 if self._newest_conn_msg != new_conn_msg:
                     print("we got a brand new message")
                     self._newest_conn_msg = new_conn_msg
-                    self._handle_conn_msg(self._newest_conn_msg)
+                    self._newest_conn_command_data, self._newest_conn_argument_data = self._handle_conn_msg(self._newest_conn_msg)
+                    self.new_message_flag = True
 
     
     def start_master(self):
         self._master_active = True
-        self.master_thread = threading.Thread(target=ConnectionManager._master, args=[self])
+        self.master_thread = threading.Thread(target=self._master)
         self.master_thread.start()
 
     def end_master(self):
@@ -43,8 +47,10 @@ class ConnectionManager(object):
             command_data, argument_data = self._parse_message(message, "<", ">", "|")
             print(f"received command: {command_data[0]}")
             print(f"receiced data {command_data}, {argument_data}")
+            return (command_data, argument_data)
         except Exception as e:
-            print(e)
+            print("The message wasn't in a valid format")
+            return [],[]
 
 
     def _parse_message(self, message, start_char, end_char, sep_char):
@@ -94,19 +100,39 @@ class ConnectionManager(object):
     def get_secure_connection(self):
         return self._secure_connection
     
+    def get_newest_message(self):
+        self.new_message_flag = False
+        return self._newest_conn_msg
+
+
+    def get_newest_message_data(self):
+        self.new_message_flag = False
+        return self._newest_conn_command_data, self._newest_conn_argument_data
+    
 class ServerManager(object):
     def __init__(self):
         self._server:ss.Server
         self._conn_managers = []
         self._master_active = False
+        self.master_thread = None
+        self._master_active = False
 
     def _master(self):
         while self._master_active:
             self._refresh_conns()
+            for conn_manager in self._conn_managers:
+                if conn_manager.new_message_flag == True:
+                    message_to_broadcast = conn_manager.get_newest_message()
+                    for recipient_conn_manager in self._conn_managers:
+                        recipient_conn_manager.send_socket_message(message_to_broadcast)
+
     
     def _refresh_conns(self):
         """makes sure self._conn_managers is up to date with the latest conns on the server"""
-        server_conns = self._server.get_conns()
+        try:
+            server_conns = self._server.get_conns()
+        except:
+            return
 
         # checks for outdated conns and removes them
         for conn_manager in self._conn_managers:
@@ -117,7 +143,9 @@ class ServerManager(object):
             
             if not conn_found:
                 print("Removing an old connection that no longer exists on the server...")
-                                
+                conn_manager.end_master()
+                self._conn_managers.remove(conn_manager)
+
 
         # checks for new conns and adds them
         for conn in server_conns:
@@ -134,13 +162,23 @@ class ServerManager(object):
                 self._conn_managers.append(new_conn_manager)
 
 
-
-
     def set_server(self, new_server):
         self._server = new_server
 
 
 
+    def start_master(self):
+        self._master_active = True
+        self.master_thread = threading.Thread(target=self._master)
+        self.master_thread.start()
+
+    def end_master(self):
+        self._master_active = False
+        if self.master_thread != None:
+            self.master_thread.join()
+
+    def shutdown_server(self):
+        self._server.set_socket_status(False)
 
 
 class Entity(object):
---
+++ testserver.py
@@ -0,0 +1,20 @@
+import entities as en
+import socket
+import securesocket as ss
+import time
+
+server = ss.Server(42067, "utf-8", "!DISCONN", "!HANDSHAKE")
+server.set_socket_status(True)
+print(server)
+
+time.sleep(0.1)
+
+
+my_server_manager = en.ServerManager()
+my_server_manager.set_server(server)
+my_server_manager.start_master()
+
+input()
+
+my_server_manager.end_master()
+my_server_manager.shutdown_server()
